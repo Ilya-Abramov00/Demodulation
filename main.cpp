c1f53753882c6f1ac3c11011c42ed23212a07194
Ana <nasti09112002@gmail.com>
2023-07-09 20:07:48 -0300

    законченные тесты и новый demodAMtest

--- main.cpp
+++ main.cpp
@@ -5,24 +5,25 @@
 #include "Base/demod.h"
 #include "Base/writer.h"
 
-int main()
-{
+int main() {
 
-   // Complex <int32_t> lolo (3, 4);
-   //std::cout << lolo.conj().im() << std::endl;
+    // Complex <int32_t> lolo (3, 4);
+    // std::cout << lolo.conj().im() << std::endl;
     Reader reader;
-    std::string ds = "/home/ann/WORK/work_qt/firstSt/demodulDD//dataset66";
-    SignalFile<float> fileM = reader.readFile<float>(ds);
- /*   for(uint32_t i=0;i<fileM.size();i++){
-        std::cout << fileM.data()[i].re() << '\t' << fileM.data()[i].im() << std::endl;
-    }
-*/
+    std::string ds = "/home/ann/WORK/work_mtlb/mtlb_demodulation/DATA_for_deAM";
+    SignalFile< float > fileM = reader.readFile< float >( ds );
+    std::cout << fileM.freqSample() << std::endl;
+    /*   for(uint32_t i=0;i<fileM.size();i++){
+           std::cout << fileM.data()[i].re() << '\t' << fileM.data()[i].im() << std::endl;
+       }
+     */
+
     Demodulation demo;
-    std:: vector<float> data = demo.demodAM(fileM);
+    std::vector< float > data = demo.demodAM( fileM );
     int64_t fd = fileM.freqSample();
     Writer writer;
-    std::string directory = "/home/ann/WORK/work_mtlb/";
-    writer.writeFile(directory, data, fd);
+    std::string directory = "/home/ann/WORK/work_mtlb/mtlb_demodulation/";
+    writer.writeFile( directory, data, fd );
     return 0;
 
 
